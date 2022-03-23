fb260b3b3bab16eb4155771403080f4b338adce2
eatttt <3395697192@qq.com>
2022-03-23 11:32:39 +0800

    pass

--- js/旅游详情.js
+++ js/旅游详情.js
@@ -1,1 +1,7 @@
-let a;
+$('.short2').eq(1).hide()
+$('.short2').eq(2).hide()
+$('.short1').click(function() {
+             $(this).addClass('mm');
+             $(this).siblings().removeClass('mm');
+             $('.short2').eq($(this).index()).show().siblings().hide()
+           })
